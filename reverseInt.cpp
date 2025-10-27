465e3d139985cdbd6a57b6cea782880a6f8a3af3
Sejal207 <guptasejal1212@gmail.com>
2025-10-28 02:30:05 +0530

    reverse int cpp

---
+++ reverseInt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>  
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+                return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int nums[] = {123, -123, 120, 1534236469};
+    for (int x : nums) {
+        cout << "Input: " << x << " -> Output: " << sol.reverse(x) << endl;
+    }
+
+    return 0;
+}
