2a405482168b651d4f67183ff5f7b38b5e97aaf4
Sejal207 <guptasejal1212@gmail.com>
2025-12-15 23:51:08 +0530

    middle node

---
+++ middleNode.cpp
@@ -0,0 +1,48 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+
+#include <iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(6);
+
+
+    ListNode* middle = sol.middleNode(head);
+
+    // Print the middle node
+    cout << "Middle node value: " << middle->val << endl;
+
+    return 0;
+}
